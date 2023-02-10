512b4de311f3eede234247df23147e5c6b9e0fd4
Julian Rivera <julianarevircoins@gmail.com>
2023-02-10 11:48:21 -0500

    populate lesson

---
+++ routes/comment.routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const User = require("../models/User.model");
+const Post = require("../models/Post.model");
+const Comment = require("../models/Comment.model");
+
+// ****************************************************************************************
+// POST route - create a comment of a specific post
+// ****************************************************************************************
+
+module.exports = router;
---
+++ seeds.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+
+const User = require('./models/User.model');
+
+// ℹ️ Connects to the database
+
+// User.collection.drop();
+
+const fakeUsers = [
+  {
+    username: 'amartin07'
+  },
+  {
+    username: 'luca85'
+  },
+  {
+    username: 'madmax'
+  }
+];
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(x => {
+    console.log(`Connected to Mongo database: "${x.connections[0].name}"`);
+
+    // Create new documents in the books collection
+    return User.create(fakeUsers)
+  })
+  .then(dbUsers => {
+    console.log(`Created ${dbUsers.length} users`);
+    return mongoose.connection.close();
+  })
+  .then(() => {
+    // Once the DB connection is closed, print a message
+    console.log('DB connection closed!');
+  })
+  .catch(err => {
+    console.log(`An error occurred while creating fake users in the DB: ${err}`);
+  });
+
+
+
